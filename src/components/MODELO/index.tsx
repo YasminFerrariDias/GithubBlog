70a8291859e1ad1d138b5975911d56bcd52a3c75
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-20 08:54:36 -0300

    Criação da página de MODELO

---
+++ src/components/MODELO/index.tsx
@@ -0,0 +1,7 @@
+import { ExemploContainer } from "./styles";
+
+export function exemplo() {
+  return (
+    <ExemploContainer>EXEMPLO</ExemploContainer>
+  )
+}
---
+++ src/components/MODELO/styles.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const ExemploContainer = styled.div``;
