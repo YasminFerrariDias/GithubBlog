0b94cb074075159ffd6b018a2798c9e5494386d1
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-20 11:00:35 -0300

    Criando o componente Header - inicio

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import ImageBackground from "../../assets/Background.png";
 
 export const HeaderContainer = styled.div`
-  background-color: pink;
-  
-
+  background-image: url(${ImageBackground});
+  height: 18.5rem;
+  width: auto;
 `;
