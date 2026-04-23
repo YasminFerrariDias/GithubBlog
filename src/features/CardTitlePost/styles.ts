76fbb4ec976dc19d7923c825ea487c340d71d8b7
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-23 15:50:33 -0300

    Adicionando hover nos links

--- src/features/CardTitlePost/styles.ts
+++ src/features/CardTitlePost/styles.ts
@@ -16,9 +16,21 @@ export const Footer = styled.div`
 `;
 
 export const LinkDecoration = styled(Link)`
-  text-decoration-line: none
+  text-decoration-line: none;
+  border-bottom: 1px solid transparent;
+  transition: 0.2s;
+
+  &:hover {
+    border-bottom: 1px solid ${({ theme }) => theme.colors.blue};
+  }
 `;
 
 export const ADecoration = styled.a`
-  text-decoration-line: none
+  text-decoration-line: none;
+  border-bottom: 1px solid transparent;
+  transition: 0.2s;
+
+  &:hover {
+    border-bottom: 1px solid ${({ theme }) => theme.colors.blue};
+  }
 `;
