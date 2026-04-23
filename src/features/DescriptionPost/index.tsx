1f76ce0c45c20928a27b6b7e920151848c8b650d
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-23 11:53:33 -0300

    Mostrando o corpo do post

--- src/features/DescriptionPost/index.tsx
+++ src/features/DescriptionPost/index.tsx
@@ -1,55 +1,14 @@
 import { Text } from "../../components/Text";
 import { Issues } from "../../hooks/useLoadIssues";
-import { DescriptionPostContainer, Description, Code, DescriptionFirst, Row, Comment } from "./styles";
+import { FormatMarkdownToReact } from "../../utils/formatMarkdownToReact";
+import { DescriptionPostContainer, Description } from "./styles";
 
 export function DescriptionPost({issue}: {issue: Issues}) {
   return (
     <DescriptionPostContainer>
       <Description>
-        <DescriptionFirst>
-          <Text variantSize="md" variantWeight="regular" variantColor="text"><strong>Programming languages all have built-in data structures, but these often differ from one language to another. 
-          </strong> This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn."
-          </Text>
-        </DescriptionFirst>
-
-        <Text variantSize="md" variantWeight="regular" variantColor="blue" children="Dynamic Typing" />
-        <div>
-          <Text variantSize="md" variantWeight="regular" variantColor="text" children="JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:" />
-        </div>
+        <FormatMarkdownToReact>{issue.body}</FormatMarkdownToReact>
       </Description>
-
-      <Code>
-        <div>
-          <Row>
-            <Text variantSize="md" variantWeight="regular" variantColor="span" children="let" />
-            <Text variantSize="md" variantWeight="regular" variantColor="text" children="foo =" />
-            <Text variantSize="md" variantWeight="regular" variantColor="blue" children="42" />
-          </Row>
-
-          <Row>
-            <Text variantSize="md" variantWeight="regular" variantColor="text" children="foo =" />
-            <Text variantSize="md" variantWeight="regular" variantColor="blue" children="'bar'" />
-          </Row>
-
-          <Row>
-            <Text variantSize="md" variantWeight="regular" variantColor="text" children="foo =" />
-            <Text variantSize="md" variantWeight="regular" variantColor="blue" children="true" />
-          </Row>
-        </div>
-        <div>
-          <Comment>
-            <Text variantSize="md" variantWeight="regular" variantColor="subtitle" children="// foo is now a number" />
-          </Comment>
-
-          <Comment>
-            <Text variantSize="md" variantWeight="regular" variantColor="subtitle" children="// foo is now a string" />
-          </Comment>
-
-          <Comment>
-            <Text variantSize="md" variantWeight="regular" variantColor="subtitle" children="// foo is now a boolean" />
-          </Comment>
-        </div>
-      </Code>
     </DescriptionPostContainer>
   )
 }
--- src/features/DescriptionPost/styles.ts
+++ src/features/DescriptionPost/styles.ts
@@ -10,25 +10,3 @@ export const DescriptionPostContainer = styled.div`
 
 export const Description = styled.div``;
 
-export const DescriptionFirst = styled.div`
-  flex-direction: row;
-  display: flex;
-  padding-bottom: 1.5rem;
-`;
-
-export const Code = styled.div`
-  background-color: ${({ theme }) => theme.colors.post};
-  margin-top: 1.5rem;
-  padding: 1rem;
-  display: flex;
-`;
-
-export const Row = styled.div`
-  display: flex; 
-  gap: 0.5rem;
-`;
-
-export const Comment = styled.div`
-  display: flex; 
-  margin-left: 3rem;
-`;
